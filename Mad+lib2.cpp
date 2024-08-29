37fe3e15546f1877f5c70032962f60e2b8a9de4a
Christopher Sandoval Terry <115430496+Chris-SandoTerry@users.noreply.github.com>
2024-08-29 13:39:25 -0500

    Update Mad+lib2.cpp
    Added a class to clean up the code

--- Mad+lib2.cpp
+++ Mad+lib2.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include<cmath>
 #include<string>
 using namespace std;
 
-int main()
-{
+class MadLibSubject {
+  public:
 	string color;
 	string name;
 	string verb;
@@ -12,34 +11,40 @@ int main()
 	string adjective;
 	string game;
 	string hobby;
+
+};
+
+int main()
+{
+	MadLibSubject subject;
 	char newline = '\n';
 
 
 
 	cout << "Enter favorite color" << endl;
-	getline(cin, color);
+	getline(cin, subject.color);
 
 	cout << "Enter a name." << endl;
-	getline(cin, name);
+	getline(cin, subject.name);
 	cout << "Enter a verb." << endl;
-	getline(cin, verb);
+	getline(cin, subject.verb);
 
 	cout << "Enter a noun." << endl;
-	getline(cin, noun);
+	getline(cin, subject.noun);
 
 	cout << "Enter an adjective." << endl;
-	getline(cin, adjective);
+	getline(cin, subject.adjective);
 
 	cout << "Enter any kind of game." << endl;
-	getline(cin, game);
+	getline(cin, subject.game);
 
 	cout << "Enter any kind of hobby" << endl;
-	getline(cin, hobby);
+	getline(cin, subject.hobby);
 
-	cout << "My shirt is " << color << "." << endl;
-	cout << "My mom " << noun << "says pushing " << name << "will make me more " << hobby << "," << "so now I  " << verb << ".\n";
-	cout << "But I also suck at " << game << "." << endl;
-	cout << "But I guess I will go cry i'm " << adjective << "." << endl;
+	cout << "My shirt is " << subject.color << "." << endl;
+	cout << "My mom " << subject.noun << " says pushing " << subject.name << " will make me more " << subject.hobby << "," << "so now I " << subject.verb << ".\n";
+	cout << "But I also suck at " << subject.game << "." << endl;
+	cout << "But I guess I will go cry i'm " << subject.adjective << "." << endl;
 
 
 	return 0;
